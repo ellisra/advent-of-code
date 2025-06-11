73cdac6debfb75bb174085744266766ec14c84c9
ellisra <robandrewellis@gmail.com>
2025-06-11 20:22:54 +0100

    Refactored layout and did day 1 of 2020

---
+++ python/2020/01.py
@@ -0,0 +1,31 @@
+def part1(report: set[int]) -> int:
+    seen = set()
+
+    for num in report:
+        complement = 2020 - num
+
+        if complement in seen:
+            return num * complement
+
+        seen.add(num)
+
+    return 0
+
+
+def part2(report: set[int]) -> int:
+    # HACK: This works but has the edge case of not checking if a number is reused
+    for num1 in report:
+        complement1 = 2020 - num1
+
+        for num2 in report:
+            complement2 = complement1 - num2
+
+            if complement2 in report:
+                return num1 * num2 * complement2
+
+    return 0
+
+
+if __name__ == "__main__":
+    report = set(map(int, [num for num in open("./inputs/01.txt")]))
+    print(part2(report))
--- python/2024/01.py
+++ python/2024/01.py
@@ -3,4 +3,4 @@ list1, list2 = map(
     map(list, zip(*[map(int, line.split()) for line in open("input.txt", "r")])),
 )
 print(f"Part 1: {sum([abs(list2[i] - list1[i]) for i in range(len(list1))])}")
-print(f"Part 2: {sum([num*list2.count(num) for num in list1])}")
+print(f"Part 2: {sum([num * list2.count(num) for num in list1])}")

4dc1e7bb25cb0acffb6d92d9f42fcb4b34c1510d
ellisra <robandrewellis@gmail.com>
2025-11-17 13:54:02 +0000

    Made rust day02 more readable

--- rust/src/year2015/day02.rs
+++ rust/src/year2015/day02.rs
@@ -8,30 +8,22 @@ pub fn run() {
 }
 
 fn split_dimensions(input: &[String]) -> Vec<Vec<i32>> {
-    input.iter()
-        .map(|line| {
-            line.split('x')
-                .filter_map(|num| num.parse::<i32>().ok())
-                .collect()
-        })
-        .collect()
+    input.iter().map(|line| {
+        line.split('x').filter_map(|num| num.parse::<i32>().ok()).collect()
+    }).collect()
 }
 
 fn part1(dimensions: &[Vec<i32>]) -> i32 {
-    dimensions.iter()
-        .map(|dim| {
-            let side_areas = vec![dim[0] * dim[1], dim[0] * dim[2], dim[2] * dim[1]];
-            2 * side_areas.iter().sum::<i32>() + side_areas.iter().min().unwrap()
-        })
-        .sum()
+    dimensions.iter().map(|dim| {
+        let side_areas = vec![dim[0] * dim[1], dim[0] * dim[2], dim[2] * dim[1]];
+        2 * side_areas.iter().sum::<i32>() + side_areas.iter().min().unwrap()
+    }).sum()
 }
 
 fn part2(dimensions: &[Vec<i32>]) -> i32 {
-    dimensions.iter()
-        .map(|dim| {
-            let mut sorted_dim = dim.clone();
-            sorted_dim.sort_unstable();
-            dim[0] * dim[1] * dim[2] + 2 * (sorted_dim[0] + sorted_dim[1])
-        })
-        .sum()
+    dimensions.iter().map(|dim| {
+        let mut sorted_dim = dim.clone();
+        sorted_dim.sort_unstable();
+        dim[0] * dim[1] * dim[2] + 2 * (sorted_dim[0] + sorted_dim[1])
+    }).sum()
 }

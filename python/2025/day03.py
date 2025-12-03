087dcfebf87fa52385c4344079813450dc4f9650
ellisra <robandrewellis@gmail.com>
2025-12-03 21:25:11 +0000

    2025 day 3 part 2 python

--- python/2025/day03.py
+++ python/2025/day03.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-TEST = [
-    [9,8,7,6,5,4,3,2,1,1,1,1,1,1,1],
-    [8,1,1,1,1,1,1,1,1,1,1,1,1,1,9],
-    [2,3,4,2,3,4,2,3,4,2,3,4,2,7,8],
-    [8,1,8,1,8,1,9,1,1,1,1,2,1,1,1],
-]
-
 
 def highest_joltage_pairs(banks: list[list[int]]) -> int:
     total = 0
@@ -22,13 +15,11 @@ def highest_joltage_twelves(banks: list[list[int]]) -> int:
     total = 0
     for bank in banks:
         digits = []
-        bank_slice = bank
         for i in range(11, -1, -1):
-            j, n = max(enumerate(bank_slice), key=lambda x: x[1])
+            j, n = max(enumerate(bank[:len(bank)-i]), key=lambda x: x[1])
             digits.append(n)
-            bank_slice = bank[j+1:-i]
-            print(digits)
-        total += sum(bank_slice)
+            bank = bank[j+1:]
+        total += int(''.join(str(d) for d in digits))
 
     return total
 
@@ -37,5 +28,5 @@ if __name__ == '__main__':
     with open('../inputs/2025/day03.txt') as file:
         banks = [[int(d) for d in line] for line in file.read().splitlines()]
 
-    # print(highest_joltage_pairs(banks))
-    print(highest_joltage_twelves(TEST))
+    print(f"Part 1: {highest_joltage_pairs(banks)}")
+    print(f"Part 2: {highest_joltage_twelves(banks)}")

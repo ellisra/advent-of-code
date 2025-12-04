d7f16ae6d9da43687af15751a2620c81aaa36fb2
ellisra <robandrewellis@gmail.com>
2025-12-04 18:52:21 +0000

    2025 day 4 part 2 python

--- python/2025/day04.py
+++ python/2025/day04.py
@@ -8,14 +8,29 @@ def get_accessible(
     grid: NDArray[np.str_],
     padded: NDArray[np.str_],
     offsets: list[tuple[int, int]],
-) -> NDArray[np.bool]:
-    num_neighbours = sum(padded[1+di:1+di+grid.shape[0], 1+dj:1+dj+grid.shape[1]] == '@' for di, dj in offsets)
+) -> NDArray[np.bool_]:
+    num_neighbours = sum(
+        padded[1+di:1+di+grid.shape[0], 1+dj:1+dj+grid.shape[1]] == '@'
+        for di, dj in offsets
+    )
 
     return (num_neighbours < 4) & (grid == '@')
 
 
-def remove_rolls(accessible: NDArray[np.bool], grid: NDArray[np.str_]) -> NDArray[np.str_]:
-    ...
+def get_accessible_multi_step(
+    grid: NDArray[np.str_],
+    padded: NDArray[np.str_],
+    offsets: list[tuple[int, int]],
+) -> int:
+    total = 0
+    accessible = get_accessible(grid, padded, offsets)
+    while accessible.any():
+        total += accessible.sum()
+        grid[accessible] = '.'
+        padded[1:-1, 1:-1] = grid
+        accessible = get_accessible(grid, padded, offsets)
+
+    return total
 
 
 if __name__ == '__main__':
@@ -26,3 +41,4 @@ if __name__ == '__main__':
     offsets = [(di, dj) for di in [-1, 0, 1] for dj in [-1, 0, 1] if (di, dj) != (0, 0)]
 
     print(f"Part 1: {get_accessible(grid, padded, offsets).sum()}")
+    print(f"Part 2: {get_accessible_multi_step(grid, padded, offsets)}")

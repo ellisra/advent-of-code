6ae9fe0f389d08fa955ac4bf1d633662ccce7e80
ellisra <robandrewellis@gmail.com>
2025-12-06 16:20:04 +0000

    Cleaned up day 6 part 2

--- python/2025/day06.py
+++ python/2025/day06.py
@@ -5,10 +5,10 @@ from math import prod
 
 def part_one(data: str) -> int:
     lines = data.splitlines()
-    operators = lines.pop(-1).split()
-    raw_nums = [line.split() for line in lines]
-    digits = [list(row) for row in zip(*raw_nums)]
-    nums = [list(map(int, row)) for row in digits]
+    operators = lines[-1].split()
+    digits = [line.split() for line in lines[:-1]]
+    nums = [list(map(int, col)) for col in zip(*digits)]
+
     total = 0
     for i, row in enumerate(nums):
         if operators[i] == '+':
@@ -19,31 +19,35 @@ def part_one(data: str) -> int:
     return total
 
 
+def part_two(data: str) -> int:
+    rows = [reversed(list(line)) for line in data.splitlines()]
+    columns = list(zip(*rows))
+
+    total = 0
+    current_operator = ''
+    current_numbers = []
+    for i, column in enumerate(columns):
+        is_empty = all(c == ' ' for c in column)
+        is_last = (i + 1 == len(columns))
+
+        if not is_empty:
+            current_operator = column[-1] if column[-1] in ('+', '*') else current_operator
+            current_numbers.append(int(''.join(column[:-1]).replace(' ', '')))
+        if is_empty or is_last:
+            if current_operator == '+':
+                total += sum(current_numbers)
+            elif current_operator == '*':
+                total += prod(current_numbers)
+
+            current_numbers = []
+            current_operator = ''
+
+    return total
+
+
 if __name__ == '__main__':
-    # with open('2025/example.txt') as file:
     with open('../inputs/2025/day06.txt') as file:
         raw_data = file.read()
 
     print(f"Part 1: {part_one(raw_data)}")
-
-    rows = [list(line) for line in raw_data.splitlines()]
-    rows = [reversed(line) for line in rows]
-    columns = [list(row) for row in zip(*rows)]
-
-    total = 0
-    operator = ''
-    nums = []
-    for i, col in enumerate(columns):
-        if not all(x == ' ' for x in col):
-            operator = col[-1] if col[-1] in ('+', '*') else operator
-            nums.append(int(''.join(col[:-1]).replace(' ', '')))
-        if all(x == ' ' for x in col) or i+1 == len(columns):
-            if operator == '+':
-                total += sum(nums)
-            elif operator == '*':
-                total += prod(nums)
-            # print(operator, nums)
-            nums = []
-            operator = ''
-
-    print(total)
+    print(f"Part 2: {part_two(raw_data)}")

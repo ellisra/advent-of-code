0fd4fa5e87fd654d5d031637b169b30812447598
ellisra <robandrewellis@gmail.com>
2025-12-07 17:07:03 +0000

    Cleaned up day 7 python approach

--- python/2025/day07.py
+++ python/2025/day07.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
 
+def propagate(grid: list[list[str | int]], i: int, j: int, value: int):
+    target = grid[i+1][j]
+    if target == '.':
+        grid[i+1][j] = value
+    elif isinstance(target, int):
+        grid[i+1][j] = target + value
+
+
+def solve(grid: list[list[str | int]]) -> tuple[int, int]:
+    n_splits = 0
+    grid_max_len = len(grid[0]) - 1
+    for i in range(len(grid) - 1):
+        for j, cell in enumerate(grid[i]):
+            if cell in ('.', '^'):
+                continue
+
+            value = cell if isinstance(cell, int) else 0
+            if grid[i+1][j] == '^':
+                n_splits += 1
+
+                propagate(grid, i, j-1, value)
+                if j < grid_max_len:
+                    propagate(grid, i, j+1, value)
+            else:
+                propagate(grid, i, j, value)
+
+        print(''.join(map(str, [f"{x:x}" if isinstance(x, int) else x for x in grid[i]])))
+
+    return n_splits, sum(x if isinstance(x, int) else 0 for x in grid[-1])
+
+
 if __name__ == '__main__':
     with open('2025/example.txt') as file:
-    # with open('../inputs/2025/day07.txt') as file:
         grid: list[list[str | int]] = [list(line) for line in file.read().splitlines()]
-        # grid[0] = ['|' if x == 'S' else x for x in grid[0]]
-        grid[0] = [1 if x == 'S' else x for x in grid[0]]
 
-    part1 = 0
-    for i, row in enumerate(grid[1:]):
-        for j, char in enumerate(row):
-            if grid[i][j] not in ('.', '^'):
-                if grid[i+1][j] == '^':
-                    part1 += 1
-
-                    if grid[i+1][j+1] == '.':
-                        grid[i+1][j+1] = grid[i][j]
-                    elif isinstance(grid[i+1][j+1], int):
-                        grid[i+1][j+1] += grid[i][j]  # type: ignore
-
-                    if grid[i+1][j-1] == '.':
-                        grid[i+1][j-1] = grid[i][j]
-                    elif isinstance(grid[i+1][j-1], int):
-                        grid[i+1][j-1] += grid[i][j]  # type: ignore
-                else:
-                    if grid[i+1][j] == '.':
-                        grid[i+1][j] = grid[i][j]
-                    elif isinstance(grid[i+1][j], int):
-                        grid[i+1][j] += grid[i][j]  # type: ignore
-
-        print(''.join(map(str, [f"{x:x}" if isinstance(x, int) else x for x in row])))
-
+    grid[0] = [1 if x == 'S' else x for x in grid[0]]
+    part1, part2 = solve(grid)
     print(f"Part 1: {part1}")
-    print(f"Part 2: {sum([x if isinstance(x, int) else 0 for x in grid[-1]])}")
+    print(f"Part 2: {part2}")

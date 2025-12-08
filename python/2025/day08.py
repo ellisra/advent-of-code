9d4bb02246a3a6d11bde48475c7ec32bf149880d
ellisra <robandrewellis@gmail.com>
2025-12-08 19:27:11 +0000

    Improved pair mapping

--- python/2025/day08.py
+++ python/2025/day08.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from collections import defaultdict
 from dataclasses import dataclass
 from typing import Callable
 
@@ -20,37 +19,17 @@ class Point:
         return cls(x, y, z)
 
 
-@dataclass
-class Path:
-    p1: Point
-    p2: Point
-    distance: int
-
-
-def get_shortest_path(flatmap: dict[tuple[int, int], int], points: list[Point]) -> Path:
-    key = min(flatmap, key=flatmap.__getitem__)
-    value = flatmap[key]
-    del flatmap[key]
-
-    return Path(points[key[0]], points[key[1]], value)
-
-
 if __name__ == '__main__':
     with open('2025/example.txt') as file:
     # with open('../inputs/2025/day08.txt') as file:
         points = [Point.from_line(line) for line in file]
 
     get_distance: Callable[[Point, Point], int] = lambda a, b: (a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2
-    distance_map = defaultdict(dict)
-
-    for i, point in enumerate(points):
-        distance_map[i] = {
-            i: get_distance(point, points[i])
-            for i in range(len(points))
-            if point != points[i]
-        }
-
-    flatmap: dict[tuple[int, int], int] = {(min(ok, ik), max(ok, ik)): v for ok, inner in distance_map.items() for ik, v in inner.items()}
+    flatmap: dict[tuple[int, int], int] = {
+        (i, j): get_distance(points[i], points[j])
+        for i in range(len(points))
+        for j in range(i + 1, len(points))
+    }
     sorted_flatmap = sorted(flatmap.items(), key=lambda x: x[1])
 
-    print(sorted_flatmap[:11])
+    print(sorted_flatmap[:10])
